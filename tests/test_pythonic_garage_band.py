855a187656a01fd3fa9f4aecfceafd7db95de953
CoreyDeJong <dejongcorey@gmail.com>
2020-05-14 23:06:09 -0700

    some test are working, still missing the last couple of requirements

--- tests/test_pythonic_garage_band.py
+++ tests/test_pythonic_garage_band.py
@@ -1,8 +1,27 @@
-from pythonic_garage_band.band import Musicians, Band
+from pythonic_garage_band.band import Musicians, Band, Guitarist, Bassist, Drummer
 
 
 def test_BandName():
     actual = Band("radiohead").name
     expected = "radiohead"
     assert expected == actual
+
+def test_GuitarName():
+    actual = Guitarist("jonny").name
+    expected = "jonny"
+    assert expected == actual
+
+# def test_GuitarName():
+#     actual = Guitarist("jonny").name
+#     expected = "jonny"
+#     assert expected == actual
+
+def test_memberlist():
+    actual = Musicians("jonny, thom, ed")
+    expected = "jonny, thom, ed"
+    assert expected == actual
+
+# def
+#     assert Band.to_list{} == []
+#     radiohead
 
